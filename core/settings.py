23024aa47602de75070229d32e3fdfeac890679d
Victor Mariani <dev13@nodetech.com.br>
2025-06-05 09:42:46 -0300

    second commit - deploy vercel

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r#p7l2&5+fr#kl&3pj$ep1mo)_@84!+gq409)zt#)qn#&cq$)v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
